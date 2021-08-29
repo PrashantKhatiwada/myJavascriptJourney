7616e7f861a2c202e6826a00bb3de5b22d7db1ba
Prashant Khatiwada <pkhatiwada58@gmail.com>
2021-08-29 10:01:44 +0545

    Added files

---
+++ 02Baiscs/03Operators.js
@@ -0,0 +1,31 @@
+// Calculating the discount percentage
+
+var markedPrice=799;
+var sellingPrice=199;
+
+var discountAmount=markedPrice-sellingPrice;
+
+var discountPercentage=(discountAmount/markedPrice)*100;
+
+displayDiscountPercentage = Math.round(discountPercentage);
+
+console.log("The percentage of discount for this item is "+ displayDiscountPercentage+"% off");
+
+
+//Calculating the simple interest and Compound Interest
+var principal = 2000;
+var rate=20;
+var time= 2;
+
+var rateByHundred=rate/100;
+
+var simpleInterest= (principal*rate*time)/100; //formula for simple interest
+
+const onePlusRateByHundredPowerTime=Math.pow(1+rateByHundred,time);
+
+var compoundInterest = Math.round(principal*((onePlusRateByHundredPowerTime)-1)); //formula for compound interest
+
+console.log(simpleInterest);
+
+console.log(compoundInterest);
+
