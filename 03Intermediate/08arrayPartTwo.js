76b1dba140dc7a5765e7ccaf16949d695b1d4ea9
Prashant Khatiwada <pkhatiwada58@gmail.com>
2021-11-08 10:49:45 +0545

    Added arrayPartTwo.js

---
+++ 03Intermediate/08arrayPartTwo.js
@@ -0,0 +1,29 @@
+// function isEven(element){
+// // if (element%2===0){
+// //     return true;
+// // }
+// // return false;
+// // return element%2===0;
+// }
+// var isEven=(element)=>{
+//      return element%2===0;
+// };
+// console.log(isEven(4));
+
+// var result=[2,4,6,8].every(isEven); // if every element is even it will be true
+// console.log(result);
+
+var isString=(element)=>{
+    if(typeof(element)==String){
+return true;
+    }
+    return false;
+}
+var stringResult=["Prashant","Urlabari","Nepal"].every(isString);
+    console.log(stringResult);
+
+
+var result=[2,4,6,8].every((e)=>{
+    return e%2===0;});
+    console.log(result);
+
