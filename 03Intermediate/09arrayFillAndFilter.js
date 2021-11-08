19cb8018a1acce86bd3caf94f11136c6295b7384
Prashant Khatiwada <pkhatiwada58@gmail.com>
2021-11-08 23:21:32 +0545

    Added some files

---
+++ 03Intermediate/09arrayFillAndFilter.js
@@ -0,0 +1,8 @@
+var testArray = [2,3,5,6,8,9]
+// console.log(testArray.fill("h")); // fills the array with h
+console.log(testArray.fill("h",2)); // fills the array with h
+
+
+const myNumber = [54,65,34,23,,65,76,45]
+const result=myNumber.filter((number)=>number!=65);
+console.log(result);
---
+++ 03Intermediate/10arraySliceAndSplice.js
@@ -0,0 +1,5 @@
+var users = ["Prashant", "Hitesh", "Kishor","BRG"];
+console.log(users.slice(users.length-1));
+
+users.splice(1,2,"HI");
+console.log(users);
