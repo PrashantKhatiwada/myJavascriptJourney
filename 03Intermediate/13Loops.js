8ca1a6f4803701a44c73cb167a3c4f1242b45bcd
Prashant Khatiwada <pkhatiwada58@gmail.com>
2021-12-15 21:41:30 +0545

    Added in Loops

--- 03Intermediate/13Loops.js
+++ 03Intermediate/13Loops.js
@@ -3,7 +3,18 @@
 // }
 
 const myStates = ["Morang", "Kathmandu", 1995, "Pokhara", "Birgunj", "Illam", "Chitwan"]
-for (let a=0;a<myStates.length;a++){
-    if(typeof myStates[a]!=='string') break;
-    console.log(myStates[a]);
-}
+// for (let a=0;a<myStates.length;a++){
+//     if(typeof myStates[a]!=='string') break;
+//     console.log(myStates[a]);
+// }
+
+// while(i<myStates.length){
+    //     ;console.log(myStates[i]);
+    //     i++;
+    // }
+    let i = 0;
+
+    do{
+        console.log(myStates[i]);
+        i++;
+    }while(i<myStates.length)
