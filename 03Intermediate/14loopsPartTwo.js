28bb37a52dce39eccb70c0a814fe3dcce2bfcb24
Prashant Khatiwada <pkhatiwada58@gmail.com>
2021-12-15 22:01:58 +0545

    Added loopsPartTwo

---
+++ 03Intermediate/14loopsPartTwo.js
@@ -0,0 +1,9 @@
+const myStates = ["Morang", "Kathmandu", 1995, "Pokhara", "Birgunj", "Illam", "Chitwan"]
+
+// var i =0;
+// for(;;){
+//     if(i<3) break;
+//     console.log(i);
+//     i++;
+// }
+myStates.forEach((s)=>(console.log(s)))
