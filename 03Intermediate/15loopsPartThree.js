5eb026a0b47b1018126afb7dec5226cbf85ebfc7
Prashant Khatiwada <pkhatiwada58@gmail.com>
2021-12-19 23:28:14 +0545

    Added some files

---
+++ 03Intermediate/15loopsPartThree.js
@@ -0,0 +1,14 @@
+const names = ["Youtube", "Facebook", "Netflix", "Instagram", "Amazon"]
+
+for(const n of names){
+    console.log(n)
+}
+
+const symbols = {
+    yt : "Youtube",
+    ig: "Instagram",
+    fb: "Facebook"
+};
+for (const n in symbols) {
+    console.log(`Key is ${n} and value is ${symbols[n]}`);
+}
